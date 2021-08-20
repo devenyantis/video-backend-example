9331e1bd185c3b2c2d8f57f49264525f3437d6e3
devenyantis <deven.yantis@certifai.ai>
2021-08-20 16:18:47 +0800

    wildcard port

--- src/main/java/com/example/videobackend/video_backend_example/MainVerticle.java
+++ src/main/java/com/example/videobackend/video_backend_example/MainVerticle.java
@@ -3,6 +3,7 @@ package com.example.videobackend.video_backend_example;
 import com.zandero.rest.RestRouter;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.Router;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,6 +14,18 @@ public class MainVerticle extends AbstractVerticle {
 	public void start(Promise<Void> startPromise) throws Exception {
 
 		Router router = RestRouter.register(vertx, new VideoEndpoint());
+
+		router.route().handler(io.vertx.ext.web.handler.CorsHandler.create(".*.")
+				.allowedMethod(io.vertx.core.http.HttpMethod.GET)
+				.allowedMethod(io.vertx.core.http.HttpMethod.POST)
+				.allowedMethod(io.vertx.core.http.HttpMethod.OPTIONS)
+				.allowedMethod(HttpMethod.DELETE)
+				.allowedMethod(io.vertx.core.http.HttpMethod.PUT)
+				.allowedHeader("Access-Control-Allow-Method")
+				.allowedHeader("Access-Control-Allow-Origin")
+				.allowedHeader("Cache-Control")
+				.allowedHeader("Pragma")
+				.allowedHeader("Content-Type"));
 
 		vertx.createHttpServer()
 				.requestHandler(router)
